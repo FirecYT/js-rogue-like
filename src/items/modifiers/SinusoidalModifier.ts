13ec1e5f646e8722666597694a356344bcd5857a
Firec <firecft@yandex.ru>
2026-01-30 10:03:29 +0300

    [М] пробелы на табы

--- src/items/modifiers/SinusoidalModifier.ts
+++ src/items/modifiers/SinusoidalModifier.ts
@@ -5,21 +5,21 @@ export class SinusoidalModifier implements Modifier {
 	id = 'sinusoidal';
 	public name = 'Sinusoidal';
 	type = 'modifier' as const;
-    private amplitude = 5;
-    private frequency = 0.5;
+	private amplitude = 5;
+	private frequency = 0.5;
 
-    apply(base: Effect): Effect {
-        let phase = 0;
-        const originalUpdate = base.update.bind(base);
-        base.update = (enemies) => {
-            const perpX = -Math.sin(base.angle);
-            const perpY = Math.cos(base.angle);
-            const offset = Math.cos(phase) * this.amplitude;
-            base.x += perpX * offset;
-            base.y += perpY * offset;
-            phase += this.frequency;
-            originalUpdate(enemies);
-        };
-        return base;
-    }
+	apply(base: Effect): Effect {
+		let phase = 0;
+		const originalUpdate = base.update.bind(base);
+		base.update = (enemies) => {
+			const perpX = -Math.sin(base.angle);
+			const perpY = Math.cos(base.angle);
+			const offset = Math.cos(phase) * this.amplitude;
+			base.x += perpX * offset;
+			base.y += perpY * offset;
+			phase += this.frequency;
+			originalUpdate(enemies);
+		};
+		return base;
+	}
 }
